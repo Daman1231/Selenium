e3e898ae6304bd3ef206c97c6c10f9370909597d
Daman1231 <daman.singh00786@gmail.com>
2022-12-07 22:31:07 -0500

    Assignment Selenium1

---
+++ src/main/java/com/Daman/learningSelenium/App.java
@@ -0,0 +1,13 @@
+package com.Daman.learningSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
